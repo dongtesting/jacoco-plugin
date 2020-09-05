e81da969a6189787c008e0bb838aac9b3f5aa2f5
Dongdong <33742205+dongtesting@users.noreply.github.com>
2020-09-05 16:15:38 +0800

    Update GitClone.java
    更新pull代码逻辑

--- src/main/java/hudson/plugins/jacoco/git/GitClone.java
+++ src/main/java/hudson/plugins/jacoco/git/GitClone.java
@@ -12,15 +12,14 @@
 package hudson.plugins.jacoco.git;
 
 import com.jcraft.jsch.Session;
+import org.apache.commons.httpclient.auth.CredentialsProvider;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.TransportConfigCallback;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.transport.JschConfigSessionFactory;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.transport.*;
 import org.eclipse.jgit.transport.OpenSshConfig.Host;
-import org.eclipse.jgit.transport.SshSessionFactory;
-import org.eclipse.jgit.transport.SshTransport;
-import org.eclipse.jgit.transport.Transport;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,16 +46,20 @@ public class GitClone {
 
         // 创建仓库目录
         final File tagPathFile = new File(directory);
-
-        if (!tagPathFile.exists()) {// 如果已存在文件夹,则表示已clone过,不需要重新clone
+        UsernamePasswordCredentialsProvider user = new UsernamePasswordCredentialsProvider("username", "password");
+        if (!tagPathFile.exists()) {// 如果目录不存在，就clone代码
             // 设置cloneCommand使用SSH的公钥认证
             final CloneCommand cloneCommand = Git.cloneRepository();
-            //不想用SSH的认证方式
-            UsernamePasswordCredentialsProvider user = new UsernamePasswordCredentialsProvider("username", "password");
+
             cloneCommand.setCredentialsProvider(user);
             // 克隆仓库
             cloneCommand.setBranch(tag).setURI(gitPath)
                     .setDirectory(tagPathFile).call();
+        }else {
+            //如果目录存在，就拉最新代码
+            Git git = new Git(new FileRepository(gitPath));
+            git.pull().setRemoteBranchName(tag).
+                    setCredentialsProvider(user).call();
         }
 
     }
